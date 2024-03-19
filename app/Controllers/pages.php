30c650e29bf970bc078dfa84e33c82e41afcdc3e
_ <_>
2024-03-19 14:08:37 +0700

    :initial commit

---
+++ app/Controllers/pages.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+class pages extends BaseController
+{
+    public function index()
+    {
+        $data =[
+            'title' => 'selamat datang | website anda'
+        ];
+        return view('pages,about', $data);
+    }
+
+    public function about()
+    {
+        $data =[
+            'title' => 'tentang kami | unipdu pres'
+        ];
+        return view('pages/about', $data);
+    }
+
+    public function home()
+    {
+        $data =[
+            'title' => 'tentang kami | unipdu pres'
+        ];
+        return view('pages/home', $data);
+    }
+}
